79b7c55ed3e4e8230b19fdc4dc153c60e1148464
최선 트라이텍 컴퓨터 <choisun@hafen.co.kr>
2022-07-28 10:13:49 +0900

    fast api example 완성

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-print('hello world')
+from fastapi import FastAPI
+import uvicorn
+
+testServer = FastAPI()
+
+@testServer.get("/")
+def test():
+    return 'testServer'
+
+@testServer.get("/data")
+def test():
+    return {'testServer': 1234}
 
 if __name__ == '__main__':
-    print('hello')
+    uvicorn.run(app="main:testServer",
+                host="localhost",
+                port=7000,
+                reload=True,
+                workers=4)
+
+
